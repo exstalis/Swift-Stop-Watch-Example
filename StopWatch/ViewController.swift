a5b8f1f3960ad6ecd30a6bf3008334e3147849c2
exstalis <exstalis@gmail.com>
2016-09-30 16:53:22 +0300

    Stop Watch
    I made simple timer  with Swift 3, using stack views

--- StopWatch/ViewController.swift
+++ StopWatch/ViewController.swift
@@ -9,17 +9,50 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var Counter = 0.0
+    var Timer = Foundation.Timer()
+    var isPlaying: Bool = false
+    @IBOutlet weak var resetButton: UIButton!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var pauseButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        timeLabel.text = String(Counter)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func resetTapped(_ sender: UIButton) {
+        Timer.invalidate()
+        isPlaying = false
+        Counter = 0
+        timeLabel.text = String(Counter)
+        playButton.isEnabled = true
+        pauseButton.isEnabled = true
+        
+    }
+    @IBAction func pauseTapped(_ sender: UIButton) {
+        playButton.isEnabled = true
+        pauseButton.isEnabled = false
+        Timer.invalidate()
+        isPlaying = false
+    }
+    @IBAction func playTapped(_ sender: UIButton) {
+        if isPlaying {
+            return
+        }
+        playButton.isEnabled = false
+        pauseButton.isEnabled = true
+        Timer = Foundation.Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(ViewController.updateTimer), userInfo: nil, repeats: true)
+        isPlaying = true
+    }
+    
+    @objc private func updateTimer(){
+        Counter = Counter + 0.1
+        timeLabel.text = String(format: "%.1f", Counter)
+    }
 }
 
